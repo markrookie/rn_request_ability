14898dd3ee1f22b189f0e036f7b538de84375314
markrookie <goubowenxxb@gmail.com>
2022-05-30 17:02:09 +0800

    add RequestAbilityHeader

---
+++ src/RequestAbilityHeader.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2018-present, SanQiu, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * author：Mark
+ * date：  2022/5/30 15:58
+ */
+
+export default class RequestAbilityHeader {
+    #header;
+
+    constructor(props) {
+        const { appId, sysLanguage } = props;
+        this.#header = {
+            'Content-Type': 'application/json',
+            'APP-ID': appId,
+            'Accept-Language': sysLanguage,
+        }
+    }
+
+    authorizedHeader(auth) {
+        return {
+            ...this.#header,
+            Authorization: auth,
+        }
+    }
+
+    unauthorizedHeader() {
+        return {...this.#header};
+    }
+}
