f2186e0070f6ea1bb7a7a026b1278638ae026077
markrookie <goubowenxxb@gmail.com>
2023-12-08 11:47:42 +0800

    更新Api Header的App-Version字段

--- src/RequestAbilityHeaderFactory.js
+++ src/RequestAbilityHeaderFactory.js
@@ -16,7 +16,7 @@ export default class RequestAbilityHeaderFactory {
             'Content-Type': 'application/json',
             'APP-ID': appId,
             'Accept-Language': sysLanguage,
-            APP_VERSION: appVersion,
+            'App-Version': appVersion,
         };
     }
 
